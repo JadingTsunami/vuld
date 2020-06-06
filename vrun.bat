06def489b68b6078d69be67946be7ca2d48d52c7
JadingTsunami <JadingTsunami@users.noreply.github.com>
2020-06-06 09:37:30 -0700

    Use other naming for modified vuld content.

--- vrun.bat
+++ vrun.bat
@@ -1,6 +1,6 @@
 @echo off
-if exist vuld_dir\vuld.exe del vuld_dir\vuld.exe
-if exist vuld_dir\vuld.wad del vuld_dir\vuld.wad
+if exist vuld_dir\vuldmod.exe del vuld_dir\vuldmod.exe
+if exist vuld_dir\vuldmod.wad del vuld_dir\vuldmod.wad
 if not exist doomhack.exe goto nodh
 
 echo WARNING!!
@@ -14,19 +14,19 @@ del doomhack.exe
 vuld
 if not errorlevel 0 goto :error
 
-if exist vuld_dir\vuld.exe goto checkwad
-if exist doom.exe copy doom.exe vuld_dir\vuld.exe
-if exist vuld_dir\vuld.exe goto checkwad
-if exist doom2.exe copy doom2.exe vuld_dir\vuld.exe
+if exist vuld_dir\vuldmod.exe goto checkwad
+if exist doom.exe copy doom.exe vuld_dir\vuldmod.exe
+if exist vuld_dir\vuldmod.exe goto checkwad
+if exist doom2.exe copy doom2.exe vuld_dir\vuldmod.exe
 
 :checkwad
-if exist vuld_dir\vuld.wad goto runvuld
-if exist doom.wad copy doom.wad vuld_dir\vuld.wad
-if exist vuld_dir\vuld.wad goto runvuld
-if exist doom2.wad copy doom2.wad vuld_dir\vuld.wad
+if exist vuld_dir\vuldmod.wad goto runvuld
+if exist doom.wad copy doom.wad vuld_dir\vuldmod.wad
+if exist vuld_dir\vuldmod.wad goto runvuld
+if exist doom2.wad copy doom2.wad vuld_dir\vuldmod.wad
 
 :runvuld
-vuld_dir\vuld.exe -file vuld_dir\vuld.wad
+vuld_dir\vuldmod.exe -file vuld_dir\vuldmod.wad
 goto exit
 
 :safeexit
